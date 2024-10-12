9fdc36734679eef51beeb7fd3c07d9bedd2cca7f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-12 17:49:43 +0300

    fixed redis connection url

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,9 @@
 const redis = require("redis");
+const config = require("../utils/config");
 
-const client = redis.createClient();
+const client = redis.createClient({
+    url: config.REDIS_URL || 'redis://redis:6379'
+});
 
 client.on('error', (err) => console.error("Redis client error", err));
 client.on('connect', () => console.log("Connected to Redis"));
