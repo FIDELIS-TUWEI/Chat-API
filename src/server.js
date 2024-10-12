043616f45108ae9accb4791bd6a1471ca2ace7bc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-12 13:16:05 +0300

    app server setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const http = require("http");
 const { Server } = require("socket.io");
-const PORT = 5000;
+const sequelize = require("./config/database");
+const config = require("./utils/config");
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -10,9 +11,23 @@ const io = new Server(server, {
     },
 });
 
+// Test database connection before starting the server
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connection established successfully.');
+
+        // Start the server only if the database connection is successful
+        app.listen(config.PORT, () => {
+            console.log(`Server running on port: ${config.PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
-    
+
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
     });
@@ -20,8 +35,4 @@ io.on('connection', (socket) => {
     socket.on('chatMessage', (message) => {
         io.emit('chatMessage', message);
     });
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
 });
