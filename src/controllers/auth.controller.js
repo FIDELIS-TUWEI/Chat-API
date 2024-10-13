8b1f257496b9e9030da22798390f25c8c447906e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-13 14:10:39 +0300

    logout controller/ route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,12 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
 const CustomError = require("../utils/CustomError");
 const { generateTokens, storeRefreshToken, setCookies } = require("../utils/generateTokens");
 const { query } = require("../../database/db");
+const config = require("../utils/config");
+const client = require("../config/redis");
 
 exports.signup = asyncHandler (async (req, res, next) => {
     const errors = validationResult(req);
@@ -43,7 +46,8 @@ exports.signup = asyncHandler (async (req, res, next) => {
 
         res.status(201).json({
             status: 'success',
-            user: {
+            message: "Signup Successful",
+            data: {
                 id: newUser.user_id,
                 username: newUser.username,
                 email: newUser.email,
@@ -57,5 +61,69 @@ exports.signup = asyncHandler (async (req, res, next) => {
 });
 
 exports.login = asyncHandler (async (req, res, next) => {
-    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    };
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ status: 'error', message: "Username or Password is required" });
+    }
+
+    try {
+        const userQuery = `
+            SELECT * FROM "User" WHERE username = $1;
+        `;
+
+        const userResult = await query(userQuery, [username]);
+        if (userResult.rows.length === 0) {
+            return next(new CustomError("Invalid Credentials", 401));
+        };
+
+        const user = userResult.rows[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return next(new CustomError("Invalid Credentials", 401));
+        };
+
+        const { accessToken, refreshToken } = generateTokens(user.user_id);
+        await storeRefreshToken(user.user_id, refreshToken);
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(200).json({
+            status: 'success',
+            message: "Login Successful",
+            data: {
+                id: user.user_id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.error("Error occured on login controller:", error);
+        return res.status(500).json({ status: 'error', message: error.message || 'Internal Server Error' });
+    }
+});
+
+exports.logout = asyncHandler (async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (refreshToken) {
+            const decoded = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
+            await client.del(`refresh_token: ${decoded.userId}`);
+        };
+
+        res.clearCookie('ca/');
+        res.clearCookie('ca/*');
+
+        res.status(200).json({ status: 'success', message: "Logged out successfully" });
+        
+    } catch (error) {
+        console.error("Error occured in logout controller:", error);
+        return res.status(500).json({ status: 'error', message: error.message || 'Internal server error' })
+    }
 });
