0e84eabf9b24dff8ffe0a3917057e984b3e499ab
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-14 12:04:21 +0300

    route to enable MFA

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signup, login, logout, refreshToken, getProfile, updateProfile } = require("../controllers/auth.controller");
+const { signup, login, logout, refreshToken, getProfile, updateProfile, enableMFA } = require("../controllers/auth.controller");
 const { protectRoute } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/enable-mfa", enableMFA);
 router.post("/logout", logout);
 router.get("/refresh-token", refreshToken);
 router.get("/profile", protectRoute, getProfile);
